e917261959f8ab2f73ebb49a1f44bd5daa1d5dff
chinnawat boonlok <boonlokchinnawat@gmail.com>
2026-07-10 11:43:01 +0700

    style: fix modal layout scrolling on desktop, scope css variables globally for portals, and improve backdrop contrast

--- src/components/goals/GoalForm.tsx
+++ src/components/goals/GoalForm.tsx
@@ -367,8 +367,6 @@ export function GoalForm({ open, onOpenChange, editingGoal }: GoalFormProps) {
             </div>
           </div>
 
-          </div>
-
           {/* Actions */}
           <div className="flex flex-col gap-3 px-8 pb-8 pt-4 border-t border-slate-100 dark:border-slate-800 bg-slate-50/30 dark:bg-slate-950/10 shrink-0">
             <button
